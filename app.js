d65570fd6207c442bb1fc980f08a824d5e49c3e1
KuntzeM <mathias.kuntze@mail.de>
2017-01-31 11:36:00 +0100

    fix bug: cross request

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ global.isRunningTranscoding = false;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'medienprojekt.dev');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
