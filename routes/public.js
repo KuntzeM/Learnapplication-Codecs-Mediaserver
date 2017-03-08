dc9ff393f6a9dfb1d408019200abff3b803b030c
KuntzeM <mathias.kuntze@mail.de>
2017-03-08 21:17:48 +0100

    fix bug

--- routes/public.js
+++ routes/public.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var config = require('./../config.json');
 var logger = require('./../functions/logger');
-var ffmpeg = require('fluent-ffmpeg');
 
 router.get('/status', function (req, res, next) {
 
